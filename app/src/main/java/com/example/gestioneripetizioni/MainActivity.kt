51ac5b8ef1628a3626213cdf0fb43d685ad9300e
Crico1702 <129075641+Crico1702@users.noreply.github.com>
2025-08-08 14:20:51 +0200

    Primo commit fede

--- app/src/main/java/com/example/gestioneripetizioni/MainActivity.kt
+++ app/src/main/java/com/example/gestioneripetizioni/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "buongiorno $name!",
+        text = "hi $name!",
         modifier = modifier
     )
 
