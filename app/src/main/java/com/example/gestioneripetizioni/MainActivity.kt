9e18eca58c69685d074eff2f1e7888f0ee5a2427
Crico1702 <129075641+Crico1702@users.noreply.github.com>
2025-08-08 15:11:45 +0200

    orario 15:11

--- app/src/main/java/com/example/gestioneripetizioni/MainActivity.kt
+++ app/src/main/java/com/example/gestioneripetizioni/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GestioneRipetizioniTheme {
-        Greeting("Spingere!")
+        Greeting("Forza Lazio!")
     }
 }
