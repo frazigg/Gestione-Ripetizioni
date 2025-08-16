d31a123fc8b179a435e8098a47d558d34ab7f9d9
Crico1702 <129075641+Crico1702@users.noreply.github.com>
2025-08-16 10:42:53 +0200

    Commenti sulla classe InsegnantiAdapter.kt

--- app/src/main/java/com/example/gestioneripetizioni/ui/Studente/InsegnantiAdapter.kt
+++ app/src/main/java/com/example/gestioneripetizioni/ui/Studente/InsegnantiAdapter.kt
@@ -8,29 +8,41 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.gestioneripetizioni.R
 import com.example.gestioneripetizioni.model.Insegnante
 
+//Adapter per la RecyclerView, mostra una lista di insegnanti
 class InsegnantiAdapter (private val onInsegnanteClick: (Insegnante) -> Unit) :
     RecyclerView.Adapter<InsegnantiAdapter.InsegnanteViewHolder>() {
 
+     //Lista che contiene i dati degli insegnanti da mostrare
     private var insegnanti = listOf<Insegnante>()
 
 
     fun aggiornaInsegnanti(nuoviInsegnanti: List<Insegnante>) {
         insegnanti = nuoviInsegnanti
+
+        //Notifica alla recyclerView che il dataset è cambiato
         notifyDataSetChanged()
     }
 
+    //Il ViewHolder rappresenta un singolo item della lista
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InsegnanteViewHolder {
+
+        //Crea la vista per un singolo item usando il layout passato
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_insegnante, parent, false)
         return InsegnanteViewHolder(view)
     }
 
+    //Collega i dati dell'insegnante e alla vista del ViewHolder
     override fun onBindViewHolder(holder: InsegnanteViewHolder, position: Int) {
         holder.bind(insegnanti[position])
     }
 
+    //Usa questo metodo per sapere quanti item deve disegnare
     override fun getItemCount() = insegnanti.size
 
+
+
+    //Contiene i riferimenti agli elementi della UI per un singolo item e li riempie con i dati dell'insegnante
     inner class InsegnanteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tvNome: TextView = itemView.findViewById(R.id.tvNome)
         private val tvMaterie: TextView = itemView.findViewById(R.id.tvMaterie)
@@ -43,7 +55,10 @@ class InsegnantiAdapter (private val onInsegnanteClick: (Insegnante) -> Unit) :
             tvOrari.text = "Orari: ${insegnante.orari.joinToString(", ")}"
             tvContatti.text = "${insegnante.email} | ${insegnante.telefono}"
 
+            //Imposta un Listener per il click sull'item della lista
             itemView.setOnClickListener {
+
+                //Quando viene cliccato esegue la funzione onInsegnanteClick per l'insegnante specificato come argomento
                 onInsegnanteClick(insegnante)
             }
         }
