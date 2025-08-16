a3badd5231c9bba4a6eeb4354e83ebc1ef0b6ba5
Crico1702 <129075641+Crico1702@users.noreply.github.com>
2025-08-16 10:23:32 +0200

    Creazione classe InsegnantiAdapter.kt + item_insegnante.xml

---
+++ app/src/main/java/com/example/gestioneripetizioni/ui/Studente/InsegnantiAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.gestioneripetizioni.ui.Studente
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gestioneripetizioni.R
+import com.example.gestioneripetizioni.model.Insegnante
+
+class InsegnantiAdapter (private val onInsegnanteClick: (Insegnante) -> Unit) :
+    RecyclerView.Adapter<InsegnantiAdapter.InsegnanteViewHolder>() {
+
+    private var insegnanti = listOf<Insegnante>()
+
+
+    fun aggiornaInsegnanti(nuoviInsegnanti: List<Insegnante>) {
+        insegnanti = nuoviInsegnanti
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InsegnanteViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_insegnante, parent, false)
+        return InsegnanteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: InsegnanteViewHolder, position: Int) {
+        holder.bind(insegnanti[position])
+    }
+
+    override fun getItemCount() = insegnanti.size
+
+    inner class InsegnanteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvNome: TextView = itemView.findViewById(R.id.tvNome)
+        private val tvMaterie: TextView = itemView.findViewById(R.id.tvMaterie)
+        private val tvOrari: TextView = itemView.findViewById(R.id.tvOrari)
+        private val tvContatti: TextView = itemView.findViewById(R.id.tvContatti)
+
+        fun bind(insegnante: Insegnante) {
+            tvNome.text = "${insegnante.nome} ${insegnante.cognome}"
+            tvMaterie.text = "Materie: ${insegnante.materie.joinToString(", ")}"
+            tvOrari.text = "Orari: ${insegnante.orari.joinToString(", ")}"
+            tvContatti.text = "${insegnante.email} | ${insegnante.telefono}"
+
+            itemView.setOnClickListener {
+                onInsegnanteClick(insegnante)
+            }
+        }
+    }
+}
