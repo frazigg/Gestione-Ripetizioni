ef9b3383971b79684b40e6ce947ea2c71df11351
Crico1702 <129075641+Crico1702@users.noreply.github.com>
2025-09-02 18:29:31 +0200

    daleeeeee

--- app/src/main/java/com/example/gestioneripetizioni/ui/Studente/InsegnantiAdapter.kt
+++ app/src/main/java/com/example/gestioneripetizioni/ui/Studente/InsegnantiAdapter.kt
@@ -3,50 +3,37 @@ package com.example.gestioneripetizioni.ui.Studente
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gestioneripetizioni.R
 import com.example.gestioneripetizioni.model.Insegnante
-import android.widget.Button
 
-//Adapter per la RecyclerView, mostra una lista di insegnanti
-class InsegnantiAdapter (
+class InsegnantiAdapter(
     private val onEmailClick: (Insegnante) -> Unit,
     private val onCallClick: (Insegnante) -> Unit,
     private val onFeedbackClick: (Insegnante) -> Unit
-): RecyclerView.Adapter<InsegnantiAdapter.InsegnanteViewHolder>() {
+) : RecyclerView.Adapter<InsegnantiAdapter.InsegnanteViewHolder>() {
 
-     //Lista che contiene i dati degli insegnanti da mostrare
     private var insegnanti = listOf<Insegnante>()
-
 
     fun aggiornaInsegnanti(nuoviInsegnanti: List<Insegnante>) {
         insegnanti = nuoviInsegnanti
-
-        //Notifica alla recyclerView che il dataset è cambiato
         notifyDataSetChanged()
     }
 
-    //Il ViewHolder rappresenta un singolo item della lista
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InsegnanteViewHolder {
-
-        //Crea la vista per un singolo item usando il layout passato
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_insegnante, parent, false)
         return InsegnanteViewHolder(view)
     }
 
-    //Collega i dati dell'insegnante e alla vista del ViewHolder
     override fun onBindViewHolder(holder: InsegnanteViewHolder, position: Int) {
         holder.bind(insegnanti[position])
     }
 
-    //Usa questo metodo per sapere quanti item deve disegnare
     override fun getItemCount() = insegnanti.size
 
-
-
-    //Contiene i riferimenti agli elementi della UI per un singolo item e li riempie con i dati dell'insegnante
     inner class InsegnanteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tvNome: TextView = itemView.findViewById(R.id.tvNome)
         private val tvMaterie: TextView = itemView.findViewById(R.id.tvMaterie)
@@ -62,14 +49,10 @@ class InsegnantiAdapter (
             tvOrari.text = "Orari: ${insegnante.orari.joinToString(", ")}"
             tvContatti.text = "${insegnante.email} | ${insegnante.telefono}"
 
-            //Imposta un Listener per il click sull'item della lista
-            itemView.setOnClickListener {
-
-                //Quando viene cliccato esegue la funzione onInsegnanteClick per l'insegnante specificato come argomento
-                btnEmail.setOnClickListener { onEmailClick(insegnante) }
-                btnChiama.setOnClickListener { onCallClick(insegnante) }
-                btnFeedback.setOnClickListener { onFeedbackClick(insegnante) }
-            }
+            btnEmail.setOnClickListener { onEmailClick(insegnante) }
+            btnChiama.setOnClickListener { onCallClick(insegnante) }
+            btnFeedback.setOnClickListener { onFeedbackClick(insegnante) }
         }
     }
 }
+
